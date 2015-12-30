61b83d4e9f6cbe8c111ab84844a1d104da748066
nipuns11 <nipuns11@users.noreply.github.com>
2015-12-29 22:53:52 -0500

    Update InstaTableViewCell.swift

--- instagramViewer/InstaTableViewCell.swift
+++ instagramViewer/InstaTableViewCell.swift
@@ -2,9 +2,7 @@
 //  InstaTableViewCell.swift
 //  instagramViewer
 //
-//  Created by nipun sharma on 2015-10-28.
-//  Copyright © 2015 nipun. All rights reserved.
-//
+
 
 import UIKit
 import Haneke
